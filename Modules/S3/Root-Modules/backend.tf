1fad0cb553a1dba873ef45d38abede77216b9fbf
pathasaradi <cparthasaradi123@gmail.com>
2026-05-11 18:00:02 +0530

    removed the unwanted file and change the file path

--- Modules/S3/Root-Modules/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform.tf.backend"   # create this once
-    key            = "s3-root/terraform.tfstate"   # path in bucket
-    region         = "ap-south-1"
-    # dynamodb_table = "terraform-locks"             # create this once
-    # encrypt        = true
-  }
-}
