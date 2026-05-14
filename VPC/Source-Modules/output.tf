c92d5f4cdb85d2e021450e8b809c3ebc1bf9af7d
pathasaradi <cparthasaradi123@gmail.com>
2026-05-14 07:28:56 +0530

    chage the output.tf

--- VPC/Source-Modules/output.tf
+++ VPC/Source-Modules/output.tf
@@ -3,14 +3,14 @@ output "vpc_id" {
   value       = aws_vpc.this.id
 }
 
-output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
+output "public_subnet_ids" {
+  description = "List of Public Subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.private.id
+output "private_subnet_ids" {
+  description = "List of Private Subnet IDs"
+  value       = aws_subnet.private[*].id
 }
 
 output "internet_gateway_id" {
